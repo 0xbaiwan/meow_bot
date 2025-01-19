eab5d5e36a1d73f4b9c2576441bf1ae216c98b5c
0xBaiwan <1827143939@qq.com>
2025-01-19 22:57:26 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,68 @@
+# Meowtopia 机器人
+
+![banner](./img/image.png)
+欢迎来到 Meowtopia！进入一个充满魔法的世界，可爱的猫咪们正在等待它们的英雄 - 你！拯救稀有猫咪，打造温馨家园，展示你的独特风格。
+
+作为 Kaia Wave 上的先驱项目，Meowtopia 让你可以拯救猫咪、攀登排行榜并领取空投。立即开始你的旅程，打造终极猫咪天堂！
+
+![tokenomic](./img/image-2.png)
+- 官网 https://meowtopia.fun/
+
+## 功能特性
+
+- **自动签到**
+- **自动玩拼图游戏**
+- **自动抽奖**
+- **自动领取挖矿奖励**
+- **自动建造装饰**
+- **支持多账号**
+- **支持使用代理**
+
+## 环境要求
+
+- 已安装 Node.js
+- `accounts.txt` 文件包含 Meowtopia 平台地址，获取方法如下：
+- 打开 Meowtopia 小程序 [https://t.me/Meowtopia_Kaiabot/](https://t.me/Meowtopia_Kaiabot/myapp?startapp=687123588)
+
+- 在游戏中找到设置/齿轮图标，点击并复制你的 ID
+    ![id](./img/image-1.png)
+
+## 安装步骤
+
+1. 克隆仓库：
+    ```sh
+    git clone https://github.com/0xbaiwan/meow_bot
+    cd meow_bot
+    ```
+
+2. 安装依赖：
+    ```sh
+    npm install
+    ```
+
+3. 在 `accounts.txt` 文件中输入你的 ID，每行一个用户：
+    ```sh
+    nano accounts.txt
+    ```
+
+4. 可选：使用代理：
+- 格式 `http://用户名:密码@IP:端口` 或 `socks5://用户名:密码@IP:端口`
+    ```sh
+    nano proxy.txt
+    ```
+
+5. 运行脚本：
+    ```sh
+    npm run start
+    ```
+
+## 购买代理（可选）
+
+- 免费静态住宅代理：
+   - [WebShare](https://www.webshare.io/?referral_code=gtw7lwqqelgu)
+   - [ProxyScrape](https://proxyscrape.com/)
+   - [MonoSans](https://github.com/monosans/proxy-list)
+- 付费高级静态住宅代理：
+   - [922proxy](https://www.922proxy.com/register?inviter_code=d6416857)
+   - [Proxy-Cheap](https://app.proxy-cheap.com/r/Pd6sqg)
+   - [Infatica](https://dashboard.infatica.io/aff.php?aff=580)
---
+++ main.js
@@ -0,0 +1,196 @@
+import { delay, readFile } from './utils/helper.js';
+import { logger, logMessages } from './utils/logger.js';
+const log = logger;
+import bedduSalaam from './utils/banner.js';
+import * as Kopi from './utils/api.js';
+
+// 检查用户信息
+async function checkingUser(token, proxy, i) {
+    try {
+        log.info(`正在检查用户信息...\n`);
+        const userFarmres = await Kopi.getUserFarm(token, proxy);
+        const farmingReward = userFarmres?.data?.farming_reward || 0;
+
+        const userInfoRes = await Kopi.getUserInfo(token, proxy);
+        const checkIn = await autoCheckin(token, userInfoRes, proxy, i)
+        const userInfo = userInfoRes?.data || { user_name: 'unknown', asset_info: { diamond: 0, gacha_ticket: 0, METO: 0 } };
+        const { user_name, asset_info } = userInfo;
+        const { diamond, gacha_ticket, METO } = asset_info;
+        log.info(`账户 ${i + 1} 的用户信息:`, { user_name, diamond, gacha_ticket, METO, farmingReward })
+        return { gacha_ticket, diamond, farmingReward };
+    } catch (error) {
+        log.info(`检查账户 ${i + 1} 信息时出错:`, error.message);
+        return { gacha_ticket: 0, diamond: 0, farmingReward: 0 };
+    }
+}
+
+// 自动签到功能
+async function autoCheckin(token, response, proxy, i) {
+    if (!response || !response.success) {
+        return;
+    }
+
+    try {
+        log.info(`正在获取每日签到信息...`);
+        const claimedReward = response.data.week_reward
+            .slice()
+            .reverse()
+            .find(daily => daily.tbl_user_7day.is_claim === 1);
+
+        const lastCheckIn = claimedReward?.tbl_user_7day?.updated_at || null;
+        const lastDay = claimedReward?.tbl_user_7day?.week_day_id || 0;
+
+        const lastDateCheckIn = new Date(lastCheckIn).toISOString().split('T')[0];
+        const currentDate = new Date().toISOString().split('T')[0];
+        log.info(`账户 #${i + 1} 上次签到时间:`, `[ ${lastDateCheckIn} ]`)
+
+        if (lastDateCheckIn !== currentDate) {
+            log.info(`正在为账户 #${i + 1} 进行每日签到...`);
+            const daily = await Kopi.dailyClaim(token, lastDay + 1, proxy);
+            log.info(`账户 #${i + 1} 每日签到结果:`, daily?.data || `第 ${lastDay + 1} 天已签到`);
+        } else {
+            log.warn(`跳过账户 #${i + 1} 的每日签到，今天已经签到过了。`);
+        }
+    } catch (error) {
+        log.error(`签到过程中出错`, error.message)
+    }
+}
+
+// 主运行函数，负责管理所有账户的自动化流程
+async function run() {
+    log.info(bedduSalaam);
+    await delay(3);
+
+    const wallets = await readFile('accounts.txt');
+    const proxies = await readFile('proxy.txt');
+
+    if (wallets.length === 0) {
+        log.error(`未找到账户 - 退出程序`);
+        return;
+    } else {
+        log.info(`正在为所有账户运行程序:`, wallets.length);
+    }
+
+    while (true) {
+        for (let i = 0; i < wallets.length; i++) {
+            const wallet = wallets[i];
+            const proxy = proxies[i % proxies.length] || null;
+
+            if (proxy) log.info(`正在使用代理运行账户 #${i + 1}:`, proxy);
+            else log.info(`正在运行账户 #${i + 1} (无代理)`);
+
+            const loginRes = await Kopi.loginUser(wallet, proxy);
+            const token = loginRes?.data?.access_token;
+            if (!token) continue;
+            let { gacha_ticket, diamond, farmingReward } = await checkingUser(token, proxy, i);
+
+            while (gacha_ticket > 0) {
+                log.info(`正在为账户 #${i + 1} 进行抽奖...`);
+                const gatchaRes = await Kopi.gatcha(token, wallet, proxy);
+                const reward = gatchaRes?.data[0] || "ZONK";
+                log.info(`账户 #${i + 1} 抽奖结果:`, reward);
+                gacha_ticket--;
+            }
+
+            if (farmingReward > 1) {
+                log.info(`正在为账户 #${i + 1} 领取农场奖励...`);
+                const farmRes = await Kopi.claimFarm(token, proxy);
+                const { idClaim, amount } = farmRes?.data || { idClaim: 0, amount: 0 };
+                log.info(`账户 #${i + 1} 领取农场奖励结果:`, { idClaim, amount });
+            }
+
+            await playingGame(token, proxy, i);
+            await upgradeDecortion(token, diamond, proxy, i);
+        }
+        log.warn(`所有账户运行完成，等待10分钟后继续...`);
+        await delay(10 * 60); // 10 minutes
+    }
+}
+
+// 玩拼图游戏功能
+async function playingGame(token, proxy, i) {
+    try {
+        log.info(`正在为账户 #${i + 1} 玩拼图游戏...`)
+        const playGameRes = await Kopi.playGame(token, proxy);
+        const current_level = playGameRes?.data?.current_level || 31;
+        log.info(`账户 #${i + 1} 当前关卡:`, current_level)
+        const move = 10
+
+        if (current_level <= 30) {
+            try {
+                const gameRes = await Kopi.submitGame(token, move, current_level, proxy);
+                log.info(`账户 #${i + 1} 游戏结果:`, gameRes)
+                const update = await Kopi.updateGame(token, proxy);
+                log.info(`账户 #${i + 1} 更新拼图到下一关:`, update?.data?.info || {})
+                await delay(1)
+            } catch (error) {
+                log.info(`账户 #${i + 1} 玩拼图时出错:`, error.message)
+            }
+        } else {
+            log.warn('该账户已达到最大关卡...')
+        }
+    } catch (error) {
+        log.info(`账户 #${i + 1} 玩游戏时出错:`, error.message)
+    }
+}
+
+// 升级装饰功能，管理房间装饰和升级
+async function upgradeDecortion(token, diamond, proxy, i) {
+    try {
+        const roomRes = await Kopi.currentRoom(token, proxy);
+        const room_id = roomRes?.data?.room_id || 0;
+        if (room_id) {
+            const availDecor = roomRes?.data?.decor;
+            const unlockDecorIds = roomRes.data.decor.flatMap(item => item.unlock_decor);
+            log.info(`账户 #${i + 1} 已解锁的装饰皮肤数量:`, unlockDecorIds.length)
+
+            const roomInfo = await Kopi.getUserRoomInfo(token, room_id, proxy);
+            if (roomInfo) {
+                const decor_limit = roomInfo?.data?.decor_limit || 100;
+                const decorIds = roomInfo.data.decors.map(decor => decor.id);
+
+                log.info(`账户 #${i + 1} 当前房间可用装饰数量:`, decorIds.length)
+                for (const decorId of decorIds) {
+
+                    const decors = await Kopi.getUserDecor(token, decorId, proxy);
+                    const list_skins = decors?.data?.list_skin || [];
+                    if (list_skins.length === 0) continue;
+
+                    for (const skin of list_skins) {
+                        if (skin.type === 1 && skin.price > diamond) continue; // 钻石不足时跳过高级装饰
+                        else if (unlockDecorIds.includes(skin.id)) continue; // 跳过已解锁的装饰
+                        log.info(`账户 #${i + 1} 正在尝试建造装饰:`, { room_id, decorId, skinId: skin.id })
+                        const build = await Kopi.buidDecor(token, decorId, skin.id, room_id, proxy);
+                        if (build === "Wait building") {
+                            log.warn(`已有待完成的建造 - 跳过...`);
+                            break;
+                        }
+                        if (skin.type === 1) {
+                            diamond -= skin.price;
+                        }
+                        log.info(`装饰建造是否成功?`, build?.success || false)
+                    };
+                }
+
+                if (decor_limit === availDecor.length) {
+                    log.info(`正在获取账户 #${i + 1} 升级房间的钻石价格...`);
+                    const getCost = await Kopi.getRoomCost(token, room_id + 1, proxy);
+                    const cost = getCost?.data[0]?.value || 0;
+                    log.info(`账户 #${i + 1} 升级房间费用:`, `${cost} 钻石 | 当前钻石: ${diamond}`);
+                    if (cost !== 0 && +cost < diamond) {
+                        log.info(`正在为账户 #${i + 1} 升级房间...`)
+                        const updateRes = await Kopi.updateRoom(token, proxy);
+                        log.info(`账户 #${i + 1} 升级结果:`, updateRes)
+                    } else {
+                        log.warn(`钻石不足，无法升级房间...`)
+                    }
+                }
+            }
+        }
+
+    } catch (error) {
+        log.info(`升级装饰时出错:`, error.message)
+    }
+}
+
+run()
